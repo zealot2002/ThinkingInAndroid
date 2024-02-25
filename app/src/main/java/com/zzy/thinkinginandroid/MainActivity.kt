0be6bfc947676fad03ccfa8d12a2412aec81b250
zzy <zzy_2002@126.com>
2024-02-25 13:47:24 +0800

    //理发店会员管理

--- app/src/main/java/com/zzy/thinkinginandroid/MainActivity.kt
+++ app/src/main/java/com/zzy/thinkinginandroid/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //理发店会员管理
+        //单机
     }
 }
